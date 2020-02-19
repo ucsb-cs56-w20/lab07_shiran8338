c873728d06f402b3d10d152da665677638bf9da2
Shiran Wang <swang02@umail.ucsb.edu>
2020-02-19 11:39:54 -0800

    sw-add  code to retrieve earthquake data from USGS web service

--- src/main/java/edu/ucsb/cs56/w20/lab07/services/EarthquakeQueryService.java
+++ src/main/java/edu/ucsb/cs56/w20/lab07/services/EarthquakeQueryService.java
@@ -7,11 +7,38 @@ public class EarthquakeQueryService {
 
     private Logger logger = LoggerFactory.getLogger(EarthquakeQueryService.class);
 
-    public String getJSON(int distance, int minmag) {
+        public String getJSON(int distance, int minmag) {
         String fakeJson = "{ \"key\": \"value\" }";
         String json = fakeJson;
         logger.info("json=" + json);
         return json;
-    }
+        RestTemplate restTemplate = new RestTemplate();
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+        String uri = "https://earthquake.usgs.gov/fdsnws/event/1/query";
+        double ucsbLat = 34.4140;
+        double ucsbLong = -119.8489;
+        String params = String.format("?format=geojson&minmagnitude=%d&maxradiuskm=%d&latitude=%f&longitude=%f",
+           minmag,distance,ucsbLat,ucsbLong);
+
+        String url = uri + params;
+        logger.info("url=" + url);
+
+        String retVal="";
+        try {   
+            ResponseEntity<String> re = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+             MediaType contentType = re.getHeaders().getContentType();
+            HttpStatus statusCode = re.getStatusCode();
+            retVal = re.getBody();
+        } catch (HttpClientErrorException e) {
+            retVal = "{\"error\": \"401: Unauthorized\"}";
+        }
+        logger.info("from EarthquakeQueryService.getJSON: " + retVal);
+        return retVal;
+    }
 }
