418d3328d4730fdb41203f4922a630eec0874aba
Shiran Wang <swang02@umail.ucsb.edu>
2020-02-19 11:09:45 -0800

    sw-pull into the master branch

--- src/main/java/edu/ucsb/cs56/w20/lab07/controllers/EarthquakesController.java
+++ src/main/java/edu/ucsb/cs56/w20/lab07/controllers/EarthquakesController.java
@@ -13,7 +13,16 @@ public class EarthquakesController {
             EqSearch eqSearch) {
         return "earthquakes/search";
     }
+     @GetMapping("/earthquakes/results")
+      public String getEarthquakesResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
+              EqSearch eqSearch) {
+          model.addAttribute("eqSearch", eqSearch);
+          // TODO: Actually do the search here and add results to the model
+          model.addAttribute("eqSearch", eqSearch);
+          return "earthquakes/results";
+      }
 
    
 }
+ 
 
